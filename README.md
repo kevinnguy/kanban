36e96be584f4146693f8b49eff9348c2328b08f6
Kevin Nguy <kevnguy@gmail.com>
2016-01-25 20:27:08 -0800

    Complete "Developing with Webpack" chapter

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Kanban - Learning React + Webpack
 
-Learning from [SurviveJS](http://survivejs.com) and at [this chapter.](http://survivejs.com/webpack_react/developing_with_webpack)
+Learning from [SurviveJS](http://survivejs.com) and at [this chapter.](http://survivejs.com/webpack_react/webpack_and_react/)
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+
+const common = {
+  // Entry accepts a path or an object of entries.
+  // The build chapter contains an example of the latter.
+  entry: PATHS.app,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      }
+    ]
+  }
+};
+
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
